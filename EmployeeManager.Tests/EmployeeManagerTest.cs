eb44f3ea4c026f83b6fece55265a3954fe53581d
Maciej Przybylski <42326412+cyvn7@users.noreply.github.com>
2024-12-01 17:35:28 +0100

    stworzony oddzielny projekt na testy e wstepnie napisanymi testami

---
+++ EmployeeManager.Tests/EmployeeManagerTest.cs
@@ -0,0 +1,69 @@
+using Xunit;
+using Moq;
+using EmployeeManager.Controllers;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+public class EmployeeControllerTest
+{
+    private readonly Mock<IEmployeeRepository> _mockRepo;
+    private readonly EmployeeController _controller;
+
+    public EmployeeControllerTest()
+    {
+        _mockRepo = new Mock<IEmployeeRepository>();
+        _controller = new EmployeeController(_mockRepo.Object);
+    }
+
+    [Fact]
+    public void Delete_EmployeeExists_ReturnsViewResultWithEmployee()
+    {
+        // Arrange
+        var employee = new Employee { 
+            Id = 1,
+            FirstName = "Jan",
+            LastName = "Kowalski",
+            PESEL = "12345678901",
+            Department = "Lorem",
+            Position = "Ipsum"
+            };
+        _mockRepo.Setup(repo => repo.GetById(1)).Returns(employee);
+
+        // Act
+        var result = _controller.Delete(1);
+
+        // Assert
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsAssignableFrom<Employee>(viewResult.ViewData.Model);
+        Assert.Equal(1, model.Id);
+    }
+
+    [Fact]
+    public void Delete_EmployeeDoesNotExist_ReturnsNotFoundResult()
+    {
+        // Arrange
+        var employeeId = 1;
+        _mockRepo.Setup(repo => repo.GetById(employeeId)).Returns((Employee)null);
+
+        // Act
+        var result = _controller.Delete(employeeId);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result);
+    }
+
+    [Fact]
+    public void DeleteConfirmed_ValidId_RedirectsToIndex()
+    {
+        // Arrange
+        var employeeId = 1;
+
+        // Act
+        var result = _controller.DeleteConfirmed(employeeId);
+
+        // Assert
+        _mockRepo.Verify(repo => repo.Delete(employeeId), Times.Once);
+        var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectToActionResult.ActionName);
+    }
+}
--- EmployeeManager/Models/Employee.cs
+++ EmployeeManager/Models/Employee.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-
 public class Employee
 {
     public int Id { get; set; }
